8b5e9fadb7fb8360b336e159a8a3392d1775b83a
ChrysostomosVakasiras <61441879+ChrysostomosVakasiras@users.noreply.github.com>
2020-02-29 16:45:10 +0100

    Updates

---
+++ precache-manifest.81cd0969d9e0c6d2be7c1b638abff426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07518a8e4a7d9a16e9217b4169e0f934",
+    "url": "/playlisting/index.html"
+  },
+  {
+    "revision": "b1e35c8413ab6494d107",
+    "url": "/playlisting/static/css/main.4346863f.chunk.css"
+  },
+  {
+    "revision": "8a954e79be87fcc7fffa",
+    "url": "/playlisting/static/js/2.e69761b2.chunk.js"
+  },
+  {
+    "revision": "82e2acb4bd55f5de284d06bde85616ed",
+    "url": "/playlisting/static/js/2.e69761b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e35c8413ab6494d107",
+    "url": "/playlisting/static/js/main.7b65c6dd.chunk.js"
+  },
+  {
+    "revision": "52c34906db7edd9c911c",
+    "url": "/playlisting/static/js/runtime-main.dcdcd090.js"
+  },
+  {
+    "revision": "822bfab7e7a346a1468a4bb8385da67a",
+    "url": "/playlisting/static/media/background_photo_desktop.822bfab7.jpg"
+  }
+]);
